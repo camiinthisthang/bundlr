4e1cc1c9ce3a52d19bc418dbbcf68c4f21cf17ac
Camila Ramos <camiinthisthang@Camilas-MacBook-Pro-3.local>
2022-05-04 16:14:58 -0700

    first sesh

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,43 @@
 import '../styles/globals.css'
+import { providers, utils } from 'ethers'
+import { WebBundlr } from '@bundlr-network/client'
+import { useState, useRef } from 'react'
+import { MainContext } from '../context'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [bundlrInstance, setBundlrInstance] = useState()
+  const [balance, setBalance] = useState();
+  const bundlrRef = useRef()
+
+  async function initialize(){
+    await window.ethereum.enable()
+    const provider = new providers.Web3Provider(window.ethereum);
+    await provider._ready();
+    const bundlr = new WebBundlr("https://node1.bundlr.network", "matic", provider);
+    await bundlr.ready();
+    bundlrRef.current = bundlr
+    setBundlrInstance(bundlr);
+
+    fetchBalance(bundlr)
+  }
+
+  async function fetchBalance() {
+    let balance = await bundlrRef.current.getLoadedBalance();
+    setBalance(balance);
+    console.log("balance:", balance.toString());
+  }
+
+  return (
+    <MainContext.Provider
+      value={{
+        initialize, balance, fetchBalance, bundlrInstance
+      }}
+    >
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </MainContext.Provider>
+  )
 }
 
 export default MyApp
